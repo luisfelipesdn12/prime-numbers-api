c93dc79d21679f8b96d1669ab97d83188a651a01
unknown <luisfelipesdn12@gmail.com>
2020-07-11 11:55:04 -0300

    Default Files Changed

--- src/test/java/com/luisfelipesdn12/primes/PrimeNumbersAPITests.java
+++ src/test/java/com/luisfelipesdn12/primes/PrimeNumbersAPITests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class PrimeNumbersAPITests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
