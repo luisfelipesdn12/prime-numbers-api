a646c7b7d4b556ff3f6d4dfeaed19db4e24cf9f6
unknown <luisfelipesdn12@gmail.com>
2020-07-11 17:45:47 -0300

    Update Test Imports

--- src/test/java/com/luisfelipesdn12/primes/service_tests/PrimeServiceTests.java
+++ src/test/java/com/luisfelipesdn12/primes/service_tests/PrimeServiceTests.java
@@ -1,9 +1,9 @@
 package com.luisfelipesdn12.primes.service_tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.List;
 
